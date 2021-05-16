eeef739e969bd046cc4628718c33f9f3860e2626
Allan Abrahão <all.ancapistan@gmail.com>
2021-05-16 13:04:59 -0300

    learning

---
+++ see/0000/test.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+
+//  ==========                 ==========
+//  ========== www.eXcript.com ==========
+//  ==========                 ==========
+
+int main(){
+    int numerosecreto = 42;
+    printf("Hello world\n");
+
+    int chute;
+
+    printf("Qual é o seu chute? ");
+    scanf("%d", &chute);
+    printf("Seu chute foi %d\n", chute);
+    
+    printf("Você acertou!\n");
+    printf("Você errou!\n");
+
+}
