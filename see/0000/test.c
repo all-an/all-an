0c2c7a88529cd3c28966d8c321a062498cc01f03
all-an <all.ancapistan@gmail.com>
2021-05-28 10:03:17 -0300

    learning

--- see/0000/test.c
+++ see/0000/test.c
@@ -1,22 +1,27 @@
 #include <stdio.h>
 
-//  ==========                 ==========
-//  ========== 	     game	   ==========
-//  ==========                 ==========
+#define TENTATIVAS 5
 
 int main(){
+
+	//  ==========                  ==========
+	//  ========== 	     game       ==========
+	//  ==========                  ==========
+	
     int numerosecreto = 42;
-    printf("Hello world\n");
+    int chute; 
+	
+    printf("Hello world\n"); 
 
-    int chute;
-
-	for(int i = 1; i <= 3; i++){
-		printf("Tentativa %d de 3\n", i);
+	for(int i = 1; i <= TENTATIVAS; i++){
+		printf("Tentativa %d de %d\n", i, TENTATIVAS);
 	    printf("Qual é o seu chute ? ");
 	    scanf("%d", &chute);
 	    printf("Seu chute foi %d\n", chute);
 
 		int acertou = (chute == numerosecreto);
+		int maior = chute > numerosecreto;
+		int menor = chute < numerosecreto;
 
 		if(acertou){
 			printf("Você acertou!\n");
