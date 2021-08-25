ad36c449630f59c5ac2843c22edc2851ee6eb948
all-an <all.ancapistan@gmail.com>
2021-08-25 15:50:15 +0100

    learning

--- 001_Alura1/Java/bytebank-composto/src/bytebank/Conta.java
+++ 001_Alura1/Java/bytebank-composto/src/bytebank/Conta.java
@@ -1,16 +1,16 @@
 package bytebank;
 
 public class Conta { //classe blueprint planta para crianção de objetos
-	double saldo;
+	private double saldo; //private limita o acesso a este atributo apenas por métodos, exemplos nos métodos abaixo
 	int agencia; //atributos
 	int numero;
-	Cliente titular;
+	Cliente titular = new Cliente();// Este código >  = new Cliente(); ja cria em tempo de execução um objeto referenciando a classe Cliente
 	
 	public void deposita(double valor) { //valor é um argumento do método, métodos lembram funções
 		this.saldo += valor; //this quer dizer para o objeto específico que chamou o metodo
 	}
 	
-	public boolean saca(double valor) {
+	public boolean saca(double valor) { //retorna um booleano e não void / nada para quem chama este método
 		if(this.saldo >= valor) {
 			this.saldo -= valor; //this mais uma vez se referindo ao objeto que chamou o metodo
 			return true;
@@ -21,7 +21,7 @@ public class Conta { //classe blueprint planta para crianção de objetos
 	
 	public boolean transfere(double valor, Conta destino) {
 		if(this.saldo >= valor) {
-			saca(valor);
+			saca(valor); 
 			destino.deposita(valor);
 			return true;
 		}
@@ -29,6 +29,8 @@ public class Conta { //classe blueprint planta para crianção de objetos
 		
 	}
 	
-	
+	public double getSaldo() { //get é convenção
+		return this.saldo;
+	}
 }
  
