e52f9759da44a591aa9732e733fb5db820c4ce88
all-an <all.ancapistan@gmail.com>
2021-09-02 11:31:04 +0100

    learning

--- 001_Alura1/C/001/adivinhacao.c
+++ 001_Alura1/C/001/adivinhacao.c
@@ -1,5 +1,7 @@
 #include <stdio.h>
 
+#define ATTEMPTS 5
+
 int main(){
 
     printf("*************\n");
@@ -12,10 +14,34 @@ int main(){
 
     int guess;
 
-    printf("Guess a number ? \n"); 
- 
-    scanf("%d", &guess); //parameters declared inside function creation //arguments go out this function
+    for( int i = 1; i <= 5; i++){
+        printf("Tentativa %d de %d \n", i, ATTEMPTS);
 
-    printf("Your guess is: %d", guess);
+        printf("Guess a number ? \n"); 
+ 
+        scanf("%d", &guess); //parameters declared inside function creation //arguments go out this function
+
+        printf("Your guess is: %d \n", guess);
+
+        int rightGuess = (guess == secretnumber);  
+
+        //printf("Right guess is %d \n", rightGuess);   //print rightGuess 0 or 1 boolean inside the int
+
+        if(rightGuess){
+            printf("Nice, you guessed it right. !\n");
+            break;
+        } else {
+            int higherGuess = (guess > secretnumber);
+            printf("You missed, try again. \n");
+            if(higherGuess){ 
+                printf("Your guess was higher than the secret number.\n");
+            }
+            if(!higherGuess){
+                printf("Your guess was lower than the secret number. \n");
+            }
+        }
+    
+    }
+    printf("Game Over ! \n");
 
 }
