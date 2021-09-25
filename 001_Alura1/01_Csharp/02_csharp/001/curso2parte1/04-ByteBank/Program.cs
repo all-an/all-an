9cd675f9ac0a540e51696463c92d9a4c5dd5df28
all-an <all.ancapistan@gmail.com>
2021-09-25 12:56:28 -0300

    learning

---
+++ 001_Alura1/01_Csharp/02_csharp/001/curso2parte1/04-ByteBank/Program.cs
@@ -0,0 +1,48 @@
+﻿using System;
+
+namespace _04_ByteBank
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            ContaCorrente contaDoBruno = new ContaCorrente();
+
+            contaDoBruno.titular = "Bruno";
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            bool resultadoDoSaque = contaDoBruno.Sacar(50);
+            Console.WriteLine(contaDoBruno.saldo);
+
+            Console.WriteLine(resultadoDoSaque);
+
+            resultadoDoSaque = contaDoBruno.Sacar(500);
+            Console.WriteLine(contaDoBruno.saldo);
+            Console.WriteLine(resultadoDoSaque);
+
+            contaDoBruno.Depositar(500);
+
+            Console.WriteLine(contaDoBruno.saldo);
+
+            ContaCorrente contaDaGabriela = new ContaCorrente();
+
+            contaDaGabriela.titular = "Gabriela";
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+
+            bool resultadoTransferência = contaDoBruno.Transferir(200, contaDaGabriela);
+
+            Console.WriteLine("Saldo do Bruno: " + contaDoBruno.saldo);
+            Console.WriteLine("Saldo da Gabriela: " + contaDaGabriela.saldo);
+
+            Console.WriteLine(resultadoTransferência);
+
+            resultadoTransferência = contaDaGabriela.Transferir(1000, contaDoBruno);
+
+            Console.WriteLine(resultadoTransferência);
+        }
+    }
+}
