5ea3e0d5182b72e52b8d42ee320b6cd9cb60940b
all-an <all.ancapistan@gmail.com>
2021-09-28 15:21:35 -0300

    learning

---
+++ 001_Alura1/SomeCoursesnCodes/01_cplusplus/001/01/jogoadivinha.cpp
@@ -0,0 +1,54 @@
+//g++ jogoadivinha.cpp -o jogoadivinha
+//./jogoadivinha
+// Simple C++ program to display "Hello World"
+
+// Header file for input output functions
+#include<iostream>
+
+using namespace std;
+
+// main function -
+// where the execution of program begins
+int main()
+{
+	setlocale(LC_ALL,"pt_BR.UTF-8");
+	// prints hello world
+	cout << "vvvvvvvvvvvvvvvvvvvvvvv" <<endl;
+	cout << "| Jogo da Adivinha‡Æo |" <<endl;
+	cout << "^^^^^^^^^^^^^^^^^^^^^^^" <<endl;
+
+	const int NUMERO_SECRETO = 42;
+	bool nao_acertou = true;
+	int tentativas = 0;
+
+	double pontos = 1000.0;
+
+	while(nao_acertou){
+		tentativas++;
+		
+		cout << "Tentativa " << tentativas << endl;
+		cout << "Chute um n£mero: ";
+		int chute;
+		cin >> chute;
+
+		double pontos_perdidos = abs(chute - NUMERO_SECRETO)/2.0;
+		pontos -= pontos_perdidos;
+		cout << "Total de pontos: " << pontos << endl;
+
+		cout << "Chutou : " << chute << endl;
+
+		bool acertou = (chute == NUMERO_SECRETO);
+		bool maior = (chute > NUMERO_SECRETO);
+
+		if(acertou){
+			cout << "Parab‚ns, acertou !" << endl;
+			nao_acertou = false;
+		}else if(maior){
+			cout << "Errou, chutou maior que o n£mero secreto !" << endl;
+		}else{
+			cout << "Errou, chutou menor que o n£mero secreto !" << endl;
+		}
+	}
+	
+	cout << "Fim !" << endl;
+}
